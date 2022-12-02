ea7c250f1f1b58cad2081d1397c6e7d92686f123
Kássio <kassiomatheus23@gmail.com>
2022-12-02 13:47:49 -0300

    02/12/22

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -34,6 +34,8 @@ const { width, height } = useWindowDimensions();
   const [ whatsapp, setWhatsapp ] = useState(undefined);
   const [ email, setEmail ] = useState(undefined);
 
+  {/*`https://api.whatsapp.com/send?phone=5575999080292&text=Ol%C3%A1%20Enilton%2C%20meu%20nome%20%C3%A9%3A%20${name}%2C%20meu%20e-mail%20%C3%A9%3A${email}%20e%20meu%20telefone%20%C3%A9%3A${whatsapp}`*/}
+
   return (
     <Popup contentStyle={ width > 991 ? { width: width / 1.2, height: height / 1.2, borderRadius: "50px" } : {width: width / 1.2, borderRadius: "50px"}} position="center center" onClose={() => {props.setPopup(false)}} open={true} >
         <Container>
@@ -58,7 +60,7 @@ const { width, height } = useWindowDimensions();
               <Nota>Nota: Ao preencher os dados eles serão enviados automaticamente para o nosso <br/>
               WhatsApp, e assim você poderá continuar com sua jornada pelo NewLead.</Nota>
 
-              <div style={{ display: "flex", justifyContent: "center"}}><Button style={{ width: width / 5, height: height / 16 }} href={`https://api.whatsapp.com/send?phone=5575999080292&text=Ol%C3%A1%20Enilton%2C%20meu%20nome%20%C3%A9%3A%20${name}%2C%20meu%20e-mail%20%C3%A9%3A${email}%20e%20meu%20telefone%20%C3%A9%3A${whatsapp}`} target="_blank" >Entrar em contato agora</Button></div>
+              <div style={{ display: "flex", justifyContent: "center"}}><Button style={{ width: width / 5, height: height / 16 }} href={`mailto:eniltonmartins14@gmail.com?subject=Contato%20New%20Lead&body=Nome%3A%20${name}%0D%0AWhatsApp%3A%20${whatsapp}%0D%0AE-mail%3A%20${email}%0D%0A%0D%0AOl%C3%A1%2C%20quero%20saber%20mais%20sobre%20a%20consultoria.`} target="_blank" onClick={() => {props.setPopup(false)}} >Entrar em contato agora</Button></div>
 
             </Form>
 
